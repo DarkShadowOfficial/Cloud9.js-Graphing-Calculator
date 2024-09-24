e9f5aece2b4aabed28c231caae09a8def723de6e
Volatile <95543040+DarkShadowOfficial@users.noreply.github.com>
2024-09-24 12:40:42 -0500

    Update script.js

--- script.js
+++ script.js
@@ -8,6 +8,8 @@ cloud.CreateCharacterClass('graph', vars => {
     cloud.Line({x: cloud.c.width/2, y: 0}, {x: cloud.c.width/2, y: cloud.c.height}, "white");
     cloud.Line({x: 0, y: cloud.c.height/2}, {x: cloud.c.width, y: cloud.c.height/2}, "white");
     vars.count > 2 ? cloud.Line({x: vars.oldx, y: vars.oldy}, {x: vars.x, y: vars.y}, vars.color) : void(0);
+    cloud.Square(10, vars.i*20 + 10, 20, vars.color);
+    cloud.Message(vars.equation, 30, vars.i*20+10, vars.color);
 }, vars => {
     vars.oldx = vars.x;
     vars.oldy = vars.y;
@@ -19,23 +21,26 @@ let equations = document.querySelector('#eqs');
 let xscale = document.querySelector('#xscale');
 let yscale = document.querySelector('#yscale');
 let btn = document.querySelector('#graph');
-function graph(eq, xs, ys) {
-    cloud.CreateCharacterFromClass('graph', {graph: eq, xscale: xs, yscale: ys, color: `rgb(${Math.random()*80 + 175}, ${Math.random()*100 + 155}, ${Math.random()*70 + 185})`});
+let i = 0;
+function graph(eq, xs, ys, equ) {
+    cloud.CreateCharacterFromClass('graph', {graph: eq, xscale: xs, yscale: ys, color: `rgb(${Math.random()*80 + 175}, ${Math.random()*100 + 155}, ${Math.random()*70 + 185})`, equation: equ, i: i});
+    i++;
 }
 let xScale = 1;
 let yScale = 1;
 btn.onclick = () => {
+    i = 0;
     xScale = xscale.value;
     yScale = yscale.value;
     cloud.objects = [];
     cloud.ctx.clearRect(0, 0, 500, 500);
     let eqs = equations.value.split(", ");
     eqs.forEach(eq => {
-        graph(x => eval(eq.toLowerCase()), xScale, yScale);
+        graph(x => eval(eq.toLowerCase()), xScale, yScale, eq);
     });
     function animate() {
         cloud.RunAll();
-        let mousex = cloud.mouse.x - 240;
+        let mousex = cloud.mouse.x - 239;
         let mousey = cloud.mouse.y - 147;
         document.querySelector('#coords').innerHTML = `(${(mousex - cloud.c.width/2)*xScale}, ${(cloud.c.height/2 - mousey)*yScale})`;
         requestAnimationFrame(animate);
